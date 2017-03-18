732956bd5ecead4deb5c7a9ee88a94aad877e83f
LothMoria <chucknoris3@gmail.com>
2017-03-18 18:45:23 -0400

    Add files via upload

---
+++ Interface.java
@@ -0,0 +1,19 @@
+package test;
+
+public interface Interface {
+	
+	public void takeTurn(int turn);
+	
+	public Boolean checkSpace();
+	
+	public void drawBoard();
+	
+	public Boolean checkWin();
+	
+	public void setPiece(String p, int i, int j);
+	
+	public void game();
+	
+	public void clearBorad();
+	
+}
